0581922f9ad73b8b3ef242349042c77cdc9faa9c
Piyushmeena11 <meenapiyush23@gmail.com>
2024-09-30 00:37:57 +0530

    Update main.py

--- main.py
+++ main.py
@@ -26,7 +26,7 @@ bot = Client("bot",
              bot_token= "7954604868:AAHCMaOzlRCurV3TRlnsatvS2o_VSxVQMW8",
              api_id= 27309858,
              api_hash= "db2420265b9b2e6602f487fa3f102ba6"
-
+            )
 
 @bot.on_message(filters.command(["start"]) & filters.user(ADMINS))
 async def account_login(bot: Client, m: Message):
